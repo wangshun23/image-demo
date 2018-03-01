d340dbdf09a114bcc92d766f79d4758a51c3e40f
colin.wang <colin.wang@thebeastshop.com>
2018-03-01 18:59:23 +0800

    no message

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <input type="file" onchange="demo(this)">
+  </div>
+</template>
+
+<script>
+import Img from './components/img'
+
+export default {
+  name: 'App',
+  components: {
+    Img
+  },
+  data() {
+    return {
+      images: []
+    }
+  }
+}
+</script>
+
+<style>
+html,body{
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  padding: 0;
+  margin: 0;
+}
+
+#app {
+  position: relative;
+  width: 750px;
+  border: 2px solid #333333;
+  overflow: hidden;
+}
+</style>
---
+++ src/components/img.vue
@@ -0,0 +1,27 @@
+<template>
+  <div :style="{ width: width + 'px', height: height + 'px', top: top + 'px', left: left + 'px'}" class="img-warp">
+    <img :src="src" width="100%">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Img',
+  data () {
+    return {
+      src: '',
+      width: 0,
+      height: 0,
+      top: 0,
+      left: 0
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .img-warp{
+    position: absolute;
+  }
+</style>
